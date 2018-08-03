5a972fe5a3d2a6ec823d6abcf2c870dab2426e30
Alba Heidari <albaheidari@gmail.com>
2018-08-03 13:58:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "ALBA"
-var height = "72"
+var height = "7"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height + 1)
+console.log(height + 4)
